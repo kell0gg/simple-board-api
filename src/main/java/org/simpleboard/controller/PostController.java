1cc6807221b261aa6f60d34b5db2c50350f30f86
dh <kell0g_g@naver.com>
2021-07-12 18:48:05 +0900

    updated

--- src/main/java/org/simpleboard/controller/PostController.java
+++ src/main/java/org/simpleboard/controller/PostController.java
@@ -87,19 +87,4 @@ public class PostController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-
-//	@PutMapping("/tutorials/{id}")
-//	public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") long id, @RequestBody Tutorial tutorial) {
-//		Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
-//
-//		if (tutorialData.isPresent()) {
-//			Tutorial _tutorial = tutorialData.get();
-//			_tutorial.setTitle(tutorial.getTitle());
-//			_tutorial.setDescription(tutorial.getDescription());
-//			_tutorial.setPublished(tutorial.isPublished());
-//			return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
-//		} else {
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		}
-//	}
 }
